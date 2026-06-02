e2210baea160632ea3dc4d213a25792427ba457f
AkamioRui <wiboworuimakaio@gmail.com>
2026-06-02 18:14:36 +0700

    optimized clone.sh

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## setting up (in chronological order)
 - clone.sh = this will clone all the other branch
-- microk8s_setup.sh = installs microk8s and **will reboot**
+- microk8s_setup_1.sh = installs microk8s and **will reboot**
 - microk8s_setup_2.sh = enable ingress and registry
 
 
--- clone.sh
+++ clone.sh
@@ -1,12 +1,6 @@
 repo=https://github.com/AkamioRui/S1-3-2-Scalable-Resilient-Microservices-with-MicroK8s.git
 images_folder=./images
-images= web-api web-server web-scrapper database analyzer
-for $image in $images; do
-    git clone -b $image $repo $images_folder/$image    
-end;
 
-# git clone -b web-api $repo $images_folder/web-api
-# git clone -b web-server $repo $images_folder/web-server
-# git clone -b web-scrapper $repo $images_folder/web-scrapper
-# git clone -b database $repo $images_folder/database
-# git clone -b analyzer $repo $images_folder/analyzer
+for image in "web-api" "web-server" "web-scrapper" "database" "analyzer"; do
+    git clone -b $image $repo $images_folder/$image    
+done
